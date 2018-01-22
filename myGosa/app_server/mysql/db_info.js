c78aea8eecb5085d1e42d7630ecd7dfd826d1f94
Edutech <Edutech@EdutechDev>
2018-01-22 10:18:20 +0900

    mysql 설정 추가

---
+++ myGosa/app_server/mysql/db_info.js
@@ -0,0 +1,35 @@
+/**
+ * 데이터베이스 접속 정보
+ */
+
+module.exports = (function(){
+	return {
+		
+		//로컬 디비
+		local: {	
+			host: 'localhost',
+			port: '3306',
+			user: 'root',
+			password: '',
+			database: ''
+		}, 
+		
+		//운영 디비 
+		real: {
+			host: 'localhost',
+			port: '3306',
+			user: 'root',
+			password: '',
+			database: ''
+		},
+		
+		//개발 디비
+		dev: {
+			host: '10.10.10.24',
+			port: '3306',
+			user: 'user_swprodg',
+			password: 'xpzmqlf',
+			database: 'gosa'
+		}
+	}
+})();
--- myGosa/app_server/routes/index.js
+++ myGosa/app_server/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
+var mysql_dbc = require('../mysql/db_con.js')();
+var connection = mysql_dbc.init();
+mysql_dbc.test(connection);
+
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
