24f070133ad8ecb6fd60f08511978dae8fff2156
Edutech <Edutech@EdutechDev>
2018-01-24 15:58:29 +0900

    쿼리 결과값 가져오기

--- myGosa/app_server/controllers/users.js
+++ myGosa/app_server/controllers/users.js
@@ -6,12 +6,13 @@ var users =  require('../models/users');
 
 module.exports.index = function(req, res){
 	var data; 
+	
+	//모델을 이용해서 쿼리를 날리는 부분 
 	users.all(function(error, results){
-		console.log(results);
-		res.json(results);
+		console.log(results);	//쿼리 결과값 콘솔 로그 직기 
+		res.render('users', {userList:results} );// 그릴 페이지 , 보낼 객체? 결과? 
 	});
-	console.log('data:'+data.toString());
-	res.render('users', {users:data} );// 그릴 페이지 , 보낼 객체? 결과? 
+	
 };
 
 /*module.exports.index = function(req, res){
