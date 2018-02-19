d8ad37131c5164ab4549e4b9ae5905f61707a06d
silqwer <silqwer@naver.com>
2018-02-19 13:40:14 +0900

    라우터 모듈화

--- myGosa/app_server/controllers/adm/main.js
+++ myGosa/app_server/controllers/adm/main.js
@@ -1,6 +1,16 @@
 /**
  * http://usejsdoc.org/
  */
+
+var users =  require('../../models/adm/users');
+
 module.exports.index = function(req, res){
-	res.render('adm/index', { title : 'Express' } );
+	res.render('adm/index', { title : '관리자 로그인' } ); 	//그릴 페이지, 보낼 객체 
+};
+
+module.exports.login = function(req, res){
+	res.render('adm/main/index', { title : '관리자 로그인 성공' } ); 	//그릴 페이지, 보낼 객체 
+	/*users.login(function(error, results){
+		res.render('adm/main/index', { title : '관리자 로그인' } ); 	//그릴 페이지, 보낼 객체 
+	});*/
 };
