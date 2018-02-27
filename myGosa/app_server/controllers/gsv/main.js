791ac4364d4b2e5c502fb3ef7fd363b54d3274b7
buddhaSon <vayu1004@gmail.com>
2018-02-28 08:16:14 +0900

    사용자 화면 생성 1. 로그인 페이지(passport 연동), 메인페이지 생성(html), 회원가입 페이지 생성(html) 2. views/gsv/에서 ejs 분리 3. style.css 수정 4. 파비콘 추가

--- myGosa/app_server/controllers/gsv/main.js
+++ myGosa/app_server/controllers/gsv/main.js
@@ -2,5 +2,26 @@
  * http://usejsdoc.org/
  */
 module.exports.index = function(req, res){
-	res.render('gsv/index', { title : 'Express' } );
+	res.render('gsv/index', { 
+		body : 'login' 
+	});
 };
+
+module.exports.join = function(req, res){
+	res.render('gsv/index', {
+		body : 'join'
+	});
+};
+
+module.exports.main = function(req, res){
+	res.render('gsv/index', {
+		body : 'main'
+	});
+};
+
+// module.exports.join = function(req, res){
+// 	res.render('gsv/index', { 
+// 		title : 'join',
+// 		contents : 'join'
+// 	} );
+// };
