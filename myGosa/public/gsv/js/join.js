4cf57daf8e68519cdd36c079d5cd38d4b4881076
buddhaSon <vayu1004@gmail.com>
2018-03-07 20:55:58 +0900

    Modified join.js and ejs files.
    1. 회원가입 페이지 js 파일 생성 및 수정
    2. 회원가입 페이지 ejs 파일 수정 (스크립토 호출 오타 수정)
    3. 사용자 공통 header.ejs 에 jquery.js 추가

--- myGosa/public/gsv/js/join.js
+++ myGosa/public/gsv/js/join.js
@@ -1,1 +1,38 @@
-console.log('what\'s up, man');
+(function() {
+	'use strict';
+
+	const Join = {
+
+		triggers : {
+			userId	: $('#formId'),
+			joinBtn : $('#joinBtn')
+		},
+
+		listeners : {
+			userId 	: {
+				focusout: function() {
+					let self = $(this);
+
+					if(self.val().trim().length !== 0) 	return false;
+
+					//ajax에서 스탑!!!! 다음 시간에 또 봐요 :) 
+
+				}
+			},
+
+			joinBtn : {
+				click: () => {
+					
+				}
+			}
+		},
+
+		binding : () => {
+			Join.triggers.userId.on(Join.listeners.userId);
+			Join.triggers.joinBtn.on(Join.listeners.joinBtn);
+		}
+	};
+
+	Join.binding();
+
+})();
