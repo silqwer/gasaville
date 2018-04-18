e5cfa4013ac0deb7ba6e485bd4f5ce36948cd747
silqwer <silqwer@naver.com>
2018-04-18 10:07:41 +0900

    사용자 레이어 팝업 띄우기

--- myGosa/public/gsv/js/main.js
+++ myGosa/public/gsv/js/main.js
@@ -5,7 +5,8 @@
 
 		triggers: {
 			apply : $('.applyBtn'),
-			cancel : $('.cancelBtn')
+			cancel : $('.cancelBtn'),
+			examName : $('.examName')
 		},
 
 		listeners: {
@@ -48,7 +49,46 @@
 						location.reload();
 					}
 				}
+			}, 
+			dimLayerPopUp : {
+				click: function(){
+					let self = $(this)[0];
+					let exam = $(self).data('exam');
+					
+					let $el = $('#commentLayer');        //레이어의 id를 $el 변수에 저장
+					let isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
+					
+					isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
+
+			        let $elWidth = ~~($el.outerWidth()),
+			            $elHeight = ~~($el.outerHeight()),
+			            docWidth = $(document).width(),
+			            docHeight = $(document).height();
+
+			        // 화면의 중앙에 레이어를 띄운다.
+			        if ($elHeight < docHeight || $elWidth < docWidth) {
+			            $el.css({
+			                marginTop: -$elHeight /2,
+			                marginLeft: -$elWidth/2
+			            })
+			        } else {
+			            $el.css({top: 0, left: 0});
+			        }
+
+			        $el.find('a.btn-layerClose').click(function(){
+			            isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
+			            return false;
+			        });
+
+			        $('.layer .dimBg').click(function(){
+			            $('.dim-layer').fadeOut();
+			            return false;
+			        });
+					
+					
+				}
 			}
+			
 		},
 
 		fn : {
@@ -75,6 +115,7 @@
 		binding: () => {
 			Main.triggers.apply.on(Main.listeners.apply);
 			Main.triggers.cancel.on(Main.listeners.cancel);
+			Main.triggers.examName.on(Main.listeners.dimLayerPopUp);
 		}
 	};
 
