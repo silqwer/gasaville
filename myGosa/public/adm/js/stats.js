0322c37286805e08d88f724ebdaf9917ebc265e6
silqwer <silqwer@naver.com>
2018-04-27 10:39:43 +0900

    관리자 통계 버튼 디자인 적용

--- myGosa/public/adm/js/stats.js
+++ myGosa/public/adm/js/stats.js
@@ -3,7 +3,8 @@
 	
   	$("#clossBtn").on({
 		click:function(){
-			//location.href = '/admin/apply/stats/closs';
+			$('.topmenu ul li').attr('class', '');
+			$(this).parents('li').attr('class', 'on');
 			$('#statsIframe').attr('src','/admin/apply/stats/closs');
 			
 		}
@@ -11,7 +12,8 @@
   	
   	$("#visitBtn").on({
 		click:function(){
-			//location.href = '/admin/apply/stats/visit';
+			$('.topmenu ul li').attr('class', '');
+			$(this).parents('li').attr('class', 'on');
 			$('#statsIframe').attr('src','/admin/apply/stats/visit');
 		}
   	
@@ -19,14 +21,16 @@
   	
   	$("#accrueBtn").on({
 		click:function(){
-			//location.href = '/admin/apply/stats/accrue';
+			$('.topmenu ul li').attr('class', '');
+			$(this).parents('li').attr('class', 'on');
 			$('#statsIframe').attr('src','/admin/apply/stats/accrue');
 		}
 	});
   	
 	$("#userBtn").on({
 		click:function(){
-			//location.href = '/admin/apply/stats/user';
+			$('.topmenu ul li').attr('class', '');
+			$(this).parents('li').attr('class', 'on');
 			$('#statsIframe').attr('src','/admin/apply/stats/user');
 		}
 	});
